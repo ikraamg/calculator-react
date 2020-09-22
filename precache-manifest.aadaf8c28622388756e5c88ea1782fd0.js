f67ff74a82095f8300d535dd6e4e3423829ba55c
Ikraam <ikraamghoor@gmail.com>
2020-09-22 12:58:35 +0200

    Updates

---
+++ precache-manifest.aadaf8c28622388756e5c88ea1782fd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3308ffbc7af572d274813bb787fd73",
+    "url": "/calculator-react/index.html"
+  },
+  {
+    "revision": "5c8ea79757b1a50f00ed",
+    "url": "/calculator-react/static/css/main.553c6a4a.chunk.css"
+  },
+  {
+    "revision": "25ff18ea3a2aecb97d15",
+    "url": "/calculator-react/static/js/2.7585af38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-react/static/js/2.7585af38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c8ea79757b1a50f00ed",
+    "url": "/calculator-react/static/js/main.778892bb.chunk.js"
+  },
+  {
+    "revision": "928bf717d83facf55a8d",
+    "url": "/calculator-react/static/js/runtime-main.f105f787.js"
+  }
+]);
