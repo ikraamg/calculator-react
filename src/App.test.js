5ba88889444d18ea7abd9662b02e625e88eb5f21
Ikraam <ikraamghoor@gmail.com>
2020-09-22 17:10:03 +0200

    Move files into components folder

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('checks if test in App is being rendered', () => {
-  const { getByText } = render(<App />);
-  const textElement = getByText(/rendered/i);
-  expect(textElement).toBeInTheDocument();
-});
