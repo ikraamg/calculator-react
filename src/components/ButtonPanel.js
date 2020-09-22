7701bd8537c18d681e241bb23014a794ea5bf38b
Ikraam <ikraamghoor@gmail.com>
2020-09-22 17:10:50 +0200

    Create ButtonPanel component

---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Button from './Button';
+
+export default function ButtonPanel() {
+  return (
+    <div>
+      <div className="gp1">
+        <Button name="AC" />
+        <Button name="+-" />
+        <Button name="%" />
+        <Button name="÷" />
+      </div>
+      <div className="gp2">
+        <Button name="7" />
+        <Button name="8" />
+        <Button name="9" />
+        <Button name="X" />
+      </div>
+      <div className="gp3">
+        <Button name="4" />
+        <Button name="5" />
+        <Button name="6" />
+        <Button name="-" />
+      </div>
+      <div className="gp4">
+        <Button name="1" />
+        <Button name="2" />
+        <Button name="3" />
+        <Button name="+" />
+      </div>
+      <div className="gp5">
+        <Button name="0" />
+        <Button name="." />
+        <Button name=" =" />
+      </div>
+    </div>
+  );
+}
