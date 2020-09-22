0df8368db807f6b8d143ce274c7d166ef43dabf9
Ikraam <ikraamghoor@gmail.com>
2020-09-22 12:59:40 +0200

    Init repo with linters and dependancies

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('checks if test in App is being rendered', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = getByText(/rendered/i);
+  expect(textElement).toBeInTheDocument();
 });
