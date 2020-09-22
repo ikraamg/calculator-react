e7660397231c9cba631dd9e0d598bac7b65edc75
Ikraam <ikraamghoor@gmail.com>
2020-09-22 17:43:29 +0200

    Fix App to function for Milestone 2 requirement

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 
-export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      result: '0',
-    };
-  }
-
-  render() {
-    const { result } = this.state;
-    return (
-      <div className="App-body">
-        <h1>Math Magicians Calculator</h1>
-        <Display result={result} />
-        <ButtonPanel />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div id="App-body">
+      <h1>Math Magicians Calculator</h1>
+      <Display />
+      <ButtonPanel />
+    </div>
+  );
 }
