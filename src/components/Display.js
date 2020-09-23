1154451c1719e2d009cf4be1279e4bd43f92d5aa
Ikraam <ikraamghoor@gmail.com>
2020-09-23 08:27:07 +0200

    Add display styles

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './display.scss';
 
 export default function Display({ result }) {
   return (
-    <div>
+    <div className="display">
       {result}
     </div>
   );
